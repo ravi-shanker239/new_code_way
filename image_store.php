be991aa5c8d57ba38ce261327b1bf4e93153404b
ravi-shanker239 <135230864+ravi-shanker239@users.noreply.github.com>
2023-07-26 19:31:23 +0500

    Create image_store.php

---
+++ image_store.php
@@ -0,0 +1,35 @@
+// app/Http/Controllers/ImageController.php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    public function upload(Request $request)
+    {
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
+            $image->storeAs('images', $imageName, 'public'); // Save the image in the "public/images" folder
+
+            // Save the image's path in the database if needed
+            // Example: $model->image_path = 'images/' . $imageName;
+            // $model->save();
+
+            // Redirect or do something else after the upload is successful
+        }
+
+        // Handle errors or other logic if needed
+    }
+}
+// config file 
+// config/filesystems.php
+'disks' => [
+    // ...
+    'public' => [
+        'driver' => 'local',
+        'root' => public_path(),
+        'url' => env('APP_URL').'/public',
+        'visibility' => 'public',
+    ],
+],
